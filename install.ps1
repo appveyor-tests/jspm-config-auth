a7452ae07f8c6f110ec18f99eaf15df29174b0fe
Alena Hrynkevich <alena@appveyor.com>
2019-08-15 11:48:05 -0700

    Wrapped npm install into cmd /c

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Install-product node 6.2.0
 node --version
-npm install jspm@0.16.54 -g --depth 0
-npm install --depth 0
+cmd /c "npm install jspm@0.16.54 -g --depth 0"
+cmd /c "npm install --depth 0"
 jspm config registries.github.auth %JSPM_GITHUB_AUTH_TOKEN%
 jspm install -y
